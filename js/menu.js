8d62700be7f3562d4ec6b9a144ab7a14c8f68540
belensieiro <sieirojb@gmail.com>
2023-10-20 00:40:43 -0300

    agrego menú hamburguesa

---
+++ js/menu.js
@@ -0,0 +1,4 @@
+document.querySelector(".mobile-menu").addEventListener("click", function() {
+    const navbarMenu = document.querySelector(".navbar-menu");
+    navbarMenu.classList.toggle("active");
+});
