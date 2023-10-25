d3af23a6ffdfb8ab06a2bbd671d1f4bc6ea51492
belensieiro <sieirojb@gmail.com>
2023-10-25 16:41:02 -0300

    Agrego animaciones en servicios

--- js/servicios.js
+++ js/servicios.js
@@ -1,3 +1,24 @@
+var elementsActivated = [];
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible && !elementsActivated[i]) {
+      reveals[i].classList.add("active");
+      elementsActivated[i] = true; // Marcar el elemento como activado
+    } else if (elementTop >= windowHeight - elementVisible) {
+      elementsActivated[i] = false; // Reiniciar el estado si el elemento no es visible
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
 function AlterMenu(menu, boolean) {
   const Menu = document.getElementById(menu);
   if (boolean == true ){
